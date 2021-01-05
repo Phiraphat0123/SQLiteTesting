e7e2a095c3371222fcb2b4b5e0a6336c7c2fac75
Zeed <Phiraphat.zim@mail.kmutt.ac.th>
2021-01-05 14:01:10 +0700

    Create DataAccess.cs

---
+++ SQLite_Complete/DataAccess_insert/DataAccess.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace DataAccess_insert
+{
+    class DataAccess
+    {
+        public static void InitializeDatabase()
+        {
+
+            using (SqliteConnection db =
+               new SqliteConnection("Filename=sqliteSample.db"))
+            {
+                db.Open();
+
+                String tableCommand = "CREATE TABLE IF NOT " +
+                    "EXISTS Customers (numberId INTEGER PRIMARY KEY, " +
+                    "name VARCHAR(50) NOT NULL)";
+
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+
+                createTable.ExecuteReader();
+            }
+        }
+        public static void AddData(string nId , string name)
+        {
+           
+            using (SqliteConnection db =
+              new SqliteConnection("Filename=sqliteSample.db"))
+            {
+                db.Open();
+
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+               
+                insertCommand.CommandText = "INSERT INTO Customers VALUES (@Idu, @name);";
+                insertCommand.Parameters.AddWithValue("@name", name);
+                insertCommand.Parameters.AddWithValue("@Idu", nId);
+
+                insertCommand.ExecuteReader();
+
+                db.Close();
+            }
+
+        }
+        public static List<String> GetData()
+        {
+            List<String> entries = new List<string>();
+
+            
+            using (SqliteConnection db =
+               new SqliteConnection("Filename=sqliteSample.db"))
+            {
+                db.Open();
+
+                SqliteCommand selectCommand = new SqliteCommand
+                    ("SELECT * from Customers", db);
+
+                SqliteDataReader query = selectCommand.ExecuteReader();
+
+                while (query.Read())
+                {
+                    entries.Add(query.GetString(0));
+                    entries.Add(query.GetString(1));
+                   
+                }
+
+                db.Close();
+            }
+
+            return entries;
+        }
+    }
+}
